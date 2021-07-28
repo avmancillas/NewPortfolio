295c4a114abd04b709a276401793d0d95f38dbd7
Avi Mancillas <avmancillas@gmail.com>
2021-07-27 22:00:08 -0500

    Had to create new repo for portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
 import './App.css';
+import AboutMe from './components/AboutMe';
+import Languages from './components/Languages';
+import Work from './components/Work';
+import Resume from './components/Resume';
+import ContactMe from './components/ContactMe'
+import Home from './components/Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Switch>
+          <Route path = "/" exact component= {Home}/>
+          <Route path = "/AboutMe" component= {AboutMe}/>
+          <Route path = "/Languages" component= {Languages}/>
+          <Route path = "/Work" component= {Work}/>
+          <Route path = "/Resume" component= {Resume}/>
+          <Route path = "/ContactMe" component= {ContactMe}/>
+        </Switch>
+        
+
+      </div>
+    </Router>
   );
 }
+
+
+
 
 export default App;
